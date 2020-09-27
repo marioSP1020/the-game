331e3e45ea433b768612715d74b25eba3219d733
marioSP1020 <mariosp1020@gmail.com>
2020-09-27 05:27:47 +0200

    Add-Item Positioning

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 console.log('Hola 😊')
 
+//Disposicion de los div
 
-const width = 16;
-const height = width;
+const width = 15;
+const height = 15;
 
 const cellCount = width * height;
 
@@ -18,3 +19,35 @@ for (let i = 0; i < cellCount; i++) {
 
 }
 
+//Posicionamiento
+
+//cannion
+
+let cannionPosition = 202
+
+const addCannion = (index) => cells[index].classList.add('cannion');
+const removeCannion = (index) => cells[index].classList.remove('cannion');
+
+addCannion(cannionPosition);
+
+//invaders
+
+for (let i = 48; i <= 116; i++) {
+
+  if (i >= 45 && i <= 56) {
+    cells[i].classList.add('invadersHigh');
+  }
+  if (i >= 63 && i <= 71) {
+    cells[i].classList.add('invadersMedium');
+  }
+  if (i >= 78 && i <= 86) {
+    cells[i].classList.add('invadersMedium');
+  }
+  if (i >= 93 && i <= 101) {
+    cells[i].classList.add('invadersLow');
+  }
+  if (i >= 108 && i <= 116) {
+    cells[i].classList.add('invadersLow');
+  }
+
+}
